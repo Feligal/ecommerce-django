4d71f61aa54f9a1c6f013545aca8a4672e0ff186
Felix Mantini <fkmantini2012@gmail.com>
2022-10-19 16:14:56 +0200

    Implementated Review and Ratings

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,6 @@
 
 
 
-from django.contrib  import messages
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from accounts.forms import RegistrationForm
--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from store.models import Product
+from store.models import Product, ReviewRating
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
@@ -8,4 +8,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name','price','stock','category','modified_date','is_available')
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ReviewRating)
 
